80b599a29394785cd65b33336253feb97d33cc53
parthNariya <parthknariya1919@gmail.com>
2024-05-18 16:50:31 +0530

    flow canvas setuo

---
+++ src/components/FlowPlayground/FlowCanvas/FlowCanvas.tsx
@@ -0,0 +1,5 @@
+export const FlowCanvas = () => {
+  return (
+    <div className="flex-[0.85] flex h-full w-full relative text-xs bg-white"></div>
+  );
+};
--- src/components/FlowPlayground/FlowPlayground.tsx
+++ src/components/FlowPlayground/FlowPlayground.tsx
@@ -1,10 +1,11 @@
+import { FlowCanvas } from "./FlowCanvas/FlowCanvas";
 import Sidebar from "./Sidebar/Sidebar";
 
 export const FlowPlayground = () => {
   return (
     <main className="flex shrink-0 h-[65%] resize-y overflow-auto min-h-40">
       <Sidebar />
-      FlowPlayground
+      <FlowCanvas />
     </main>
   );
 };
