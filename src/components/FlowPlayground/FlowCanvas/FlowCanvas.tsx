90a195dd61e2a8658a2888d829f93e436e73e2ef
parthNariya <parthknariya1919@gmail.com>
2024-05-18 17:09:34 +0530

    react flow setup

--- src/components/FlowPlayground/FlowCanvas/FlowCanvas.tsx
+++ src/components/FlowPlayground/FlowCanvas/FlowCanvas.tsx
@@ -1,5 +1,19 @@
+import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
+import "reactflow/dist/style.css";
+import { miniMapStyle } from "./utils";
 export const FlowCanvas = () => {
   return (
-    <div className="flex-[0.85] flex h-full w-full relative text-xs bg-white"></div>
+    <div className="flex-[0.85] flex h-full w-full relative text-xs">
+      <ReactFlow fitView>
+        <MiniMap
+          style={miniMapStyle}
+          zoomable
+          pannable
+          className="bg-primary-light"
+        />
+        <Controls />
+        <Background gap={16} />
+      </ReactFlow>
+    </div>
   );
 };
---
+++ src/components/FlowPlayground/FlowCanvas/utils.ts
@@ -0,0 +1,6 @@
+import { CSSProperties } from "react";
+
+export const miniMapStyle: CSSProperties = {
+  height: 100,
+  width: 150,
+};
