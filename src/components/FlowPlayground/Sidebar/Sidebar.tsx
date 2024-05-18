488532d50df8b9542ed642707e00d92119c331d5
parthNariya <parthknariya1919@gmail.com>
2024-05-18 16:42:40 +0530

    sidebar design

--- src/components/FlowPlayground/Sidebar/Sidebar.tsx
+++ src/components/FlowPlayground/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
-import { SidebarCategoryTypeEnum } from "./type";
+import { SidebarItem } from "./SidebarItem";
+import { sidebarMenuItems } from "./data";
 
 export default function Sidebar() {
-  const category = Object.keys(SidebarCategoryTypeEnum);
   return (
     <aside className="flex flex-col flex-[0.18] border-r border-primary-dark">
       <div className="flex items-center justify-start px-2 py-1 h-7">
         <span className="text-md font-bold ml-1">Block Library</span>
       </div>
-      {category.map(item => <div key={item}>{item}</div>)}
+      {sidebarMenuItems.map((item) => (
+        <SidebarItem item={item} key={item.type} />
+      ))}
     </aside>
   );
 }
--- src/components/FlowPlayground/Sidebar/SidebarItem.tsx
+++ src/components/FlowPlayground/Sidebar/SidebarItem.tsx
@@ -1,18 +1,23 @@
-import { SidebarMenuItemDetailsType } from "./type";
+import { SidebarMenuType } from "./type";
 
 type PropType = {
-  item: SidebarMenuItemDetailsType;
+  item: SidebarMenuType;
 };
 
 export const SidebarItem = ({ item }: PropType) => {
   return (
-    <div className="block-type shadow-md p-3 mb-3 rounded-md bg-primary-dark flex flex-col gap-2">
-      <div className="font-bold capitalize text-[14px]">{item.label}</div>
-      <div className="text-[10px] opacity-75">{item.detail}</div>
-      <div className="flex flex-col mt-2">
-        <div className="text-xs opacity-75">Input: {item.input}</div>
-        <div className="text-xs opacity-75">Output: {item.input}</div>
-      </div>
+    <div className="p-2 overflow-auto">
+      <div className="font-bold mb-1">{item.type}</div>
+      {item.details.map((detail) => (
+        <div className="block-type shadow-md p-3 mb-3 rounded-md bg-primary-dark flex flex-col gap-2">
+          <div className="font-bold capitalize text-[14px] select-none">{detail.label}</div>
+          <div className="text-[10px] opacity-75 select-none">{detail.detail}</div>
+          <div className="flex flex-col mt-2">
+            <div className="text-xs opacity-75">Input: {detail.input}</div>
+            <div className="text-xs opacity-75">Output: {detail.output}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
