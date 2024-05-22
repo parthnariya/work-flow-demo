5f7eafc4e79135a0bda226cb4a2bcb0d2c86f1a2
parthNariya <parthknariya1919@gmail.com>
2024-05-22 16:06:58 +0530

    deploy issue

--- src/utils/exportData.ts
+++ src/utils/exportData.ts
@@ -3,7 +3,7 @@ import { FileData } from "../store/types";
 export function jsonToCsv(items: FileData) {
   const header = Object.keys(items[0]);
   const headerString = header.join(",");
-  const replacer = (key: string, value: string) => value ?? "";
+  const replacer = (_key: string, value: string) => value ?? "";
   const rowItems = items.map((row) =>
     header
       .map((fieldName) => JSON.stringify(row[fieldName], replacer))
